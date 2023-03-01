458b149f42c4431f4817d37f363b1b36f255c8e2
domenicocinque <domenico.cinque98@gmail.com>
2023-03-01 11:33:21 +0100

    update configs

--- main.py
+++ main.py
@@ -3,10 +3,8 @@
 To run: python backbone_image_classifier.py --trainer.max_epochs=50
 """
 import logging
-import pprint
 import warnings
 from os import path
-from typing import Optional
 
 import hydra
 import omegaconf
@@ -22,16 +20,14 @@ from torchvision import transforms
 
 log = logging.getLogger(__name__)
 
-DATASETS_PATH = path.join(path.dirname(__file__), "..", "data")
 
-
-@hydra.main(config_path="configs/", config_name="default.yaml")
+@hydra.main(version_base="1.3", config_path="configs/", config_name="default.yaml")
 def main(cfg):
     warnings.filterwarnings("ignore", ".*does not have many workers.*")
 
-    logger: WandbLogger = hydra.utils.instantiate(cfg.logger)
+    logger = hydra.utils.instantiate(cfg.logger)
     datamodule = hydra.utils.instantiate(cfg.data)
-    model: LightningModule = hydra.utils.instantiate(cfg.model)
+    model = hydra.utils.instantiate(cfg.model)
     trainer = hydra.utils.instantiate(cfg.trainer, logger=logger)
 
     trainer.fit(model=model, datamodule=datamodule)
