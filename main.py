1eac8c87645b206f88c12de558a7a13746aae073
domenicocinque <domenico.cinque98@gmail.com>
2023-03-01 11:35:35 +0100

    remove unnecessary imports

--- main.py
+++ main.py
@@ -4,19 +4,8 @@ To run: python backbone_image_classifier.py --trainer.max_epochs=50
 """
 import logging
 import warnings
-from os import path
 
 import hydra
-import omegaconf
-import torch
-import wandb
-from pytorch_lightning import LightningDataModule, LightningModule
-from pytorch_lightning.cli import LightningCLI
-from pytorch_lightning.demos.mnist_datamodule import MNIST
-from pytorch_lightning.loggers import WandbLogger
-from torch.nn import functional as F
-from torch.utils.data import DataLoader, random_split
-from torchvision import transforms
 
 log = logging.getLogger(__name__)
 
